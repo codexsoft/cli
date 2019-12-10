f4b1513c289de0b0aaf04be8c2d439741395b930
Dmitriy V Kozubskiy <kozubsky@inbox.ru>
2019-12-11 06:07:15 +0700

    initial commit

---
+++ src/Cli.php
@@ -0,0 +1,49 @@
+<?php /** @noinspection PhpUnused */
+
+namespace CodexSoft\Cli;
+
+class Cli
+{
+
+    /**
+     * todo: this nasty method should be reviewed, as it is used in a very speciefic case!
+     * @return string
+     */
+    public static function getFirstArgumentOrDie(): string
+    {
+        if (PHP_SAPI !== 'cli') {
+            die('Configuration loading works only from CLI');
+        }
+
+        if (\count($_SERVER['argv']) < 2) {
+            die("config file is required CLI argument\n");
+        }
+
+        $ormConfigFile = \realpath($_SERVER['argv'][1]);
+        if (!file_exists($ormConfigFile)) {
+            die("config file $ormConfigFile does not exists!\n");
+        }
+
+        unset($_SERVER['argv'][1]);
+
+        return $ormConfigFile;
+    }
+
+    /**
+     * This will execute $cmd in the background (no cmd window) without PHP waiting for it to
+     * finish, on both Windows and Unix.
+     *
+     * https://www.php.net/manual/en/function.exec.php#86329
+     *
+     * @param $cmd
+     */
+    public static function execInBackground($cmd): void
+    {
+        if (\strpos(php_uname(), 'Windows') === 0) {
+            pclose(popen('start /B '.$cmd, 'r'));
+        } else {
+            exec($cmd.' > /dev/null &');
+        }
+    }
+
+}
---
+++ src/Command/ExecuteShellCommand.php
@@ -0,0 +1,53 @@
+<?php /** @noinspection PhpUnused */
+
+namespace CodexSoft\Cli\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Command for generating new blank migration classes
+ */
+class ExecuteShellCommand extends Command
+{
+
+    /** @var string[] */
+    private $cmds;
+
+    /** @var bool */
+    private $stopOnError;
+
+    public function __construct(array $cmds, string $name = null, $stopOnError = true)
+    {
+        parent::__construct($name);
+        $this->cmds = $cmds;
+        $this->setDescription('Executes shell scripts');
+        $this->stopOnError = $stopOnError;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return int|void
+     * @throws \Exception
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        foreach ($this->cmds as $cmd) {
+            $out = [];
+            $output->writeln("Executing script $cmd");
+            exec(escapeshellcmd($cmd), $out, $code);
+
+            foreach($out as $line) {
+                $output->writeln($line, OutputInterface::VERBOSITY_VERY_VERBOSE);
+            }
+
+            if (((int) $code !== 0) && $this->stopOnError) {
+                throw new \Exception('Command '.$cmd.' failed with exit code '.$code.'. Further commands executing cancelled.');
+            }
+        }
+    }
+
+}
